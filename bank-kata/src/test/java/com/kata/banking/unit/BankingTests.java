9bb5b36ac97938e5b73551367b2b6be0cc13dc1e
Yoan Thirion <yoan.thirion@gmail.com>
2022-04-19 17:17:27 +0200

    add bank kata with cucumber feature

---
+++ bank-kata/src/test/java/com/kata/banking/unit/BankingTests.java
@@ -0,0 +1,12 @@
+package com.kata.banking.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BankingTests {
+    @Test
+    void test1() {
+        assertThat(1).isEqualTo(1);
+    }
+}
