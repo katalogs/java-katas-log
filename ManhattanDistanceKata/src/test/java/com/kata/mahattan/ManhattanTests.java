33ca89cb29bc31de67adf380f0310680bbf14b5e
Yoan Thirion <yoan.thirion@gmail.com>
2022-04-19 12:43:04 +0200

    add manhattan kata

---
+++ ManhattanDistanceKata/src/test/java/com/kata/mahattan/ManhattanTests.java
@@ -0,0 +1,13 @@
+package com.kata.mahattan;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ManhattanTests {
+    @Test
+    void test1() {
+        assertThat(true)
+                .isTrue();
+    }
+}
