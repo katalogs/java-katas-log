d554e2e385137f87df892f3ff230c770db1be01d
Yoan Thirion <yoan.thirion@gmail.com>
2022-04-19 12:57:59 +0200

    add mars rover

---
+++ mars-rover-kata/src/main/java/com/kata/mars/Rover.java
@@ -0,0 +1,4 @@
+package com.kata.mars;
+
+public class Rover {
+}
---
+++ mars-rover-kata/src/test/java/com/kata/mars/RoverTests.java
@@ -0,0 +1,13 @@
+package com.kata.mars;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RoverTests {
+    @Test
+    void test() {
+        assertThat(true)
+                .isFalse();
+    }
+}
