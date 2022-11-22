cf8f8caaf6e98881b13309ce5cf8d6385a1fe340
yasmine.amara <yasmine.amara@cdiscount.com>
2022-11-22 11:36:10 +0100

    refacto

--- roman-numeral-kata/src/main/java/com/kata/roman/ArabicToRomanNumeralsConverter.java
+++ roman-numeral-kata/src/main/java/com/kata/roman/ArabicToRomanNumeralsConverter.java
@@ -5,20 +5,33 @@ public class ArabicToRomanNumeralsConverter {
   private static final String V = "V";
   private static final String X = "X";
   public static final String I = "I";
+  public static final String L = "L";
 
   static String convert(int arabicNumber) {
     String result = "";
 
-    if (arabicNumber >= 1) {
-      result = I + I.repeat(arabicNumber - 1);
+    int fifties = arabicNumber / 50;
+    if (fifties >= 1) {
+      result += L.repeat(fifties);
+      arabicNumber -= fifties * 50;
     }
 
-    if (arabicNumber >= 5) {
-      result = V + I.repeat(arabicNumber - 5);
+    int tens = arabicNumber / 10;
+    if (tens >= 1) {
+      result += X.repeat(tens);
+      arabicNumber -= tens * 10;
     }
 
-    if (arabicNumber >= 10) {
-      result = X + I.repeat(arabicNumber - 10);
+    int fives = arabicNumber / 5;
+    if (fives >= 1) {
+      result += V.repeat(fives);
+      arabicNumber -= fives * 5;
+    }
+
+    int one = arabicNumber / 1;
+    if (one >= 1) {
+      result += I.repeat(one);
+      arabicNumber -= one * 1;
     }
 
     return result;
--- roman-numeral-kata/src/test/java/com/kata/roman/ArabicToRomanNumeralsConverterTests.java
+++ roman-numeral-kata/src/test/java/com/kata/roman/ArabicToRomanNumeralsConverterTests.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 class ArabicToRomanNumeralsConverterTests {
 
   @ParameterizedTest
-  @CsvSource({"1,I", "2,II", "3,III"})
+  @CsvSource({"1,I", "2,II", "3,III", "11,XI", "12,XII", "13,XIII"})
   void shouldConvertSuccessiveI(int arabicNumber, String romanNumber) {
     assertThat(ArabicToRomanNumeralsConverter.convert(arabicNumber)).isEqualTo(romanNumber);
   }
@@ -20,9 +20,14 @@ class ArabicToRomanNumeralsConverterTests {
   }
 
   @ParameterizedTest
-  @CsvSource({"10,X", "11,XI", "12,XII", "13,XIII"})
+  @CsvSource({"10,X", "20,XX", "30,XXX", "22,XXII", "27,XXVII"})
   void shouldConvertSuccessiveX(int arabicNumber, String romanNumber) {
     assertThat(ArabicToRomanNumeralsConverter.convert(arabicNumber)).isEqualTo(romanNumber);
   }
 
+  @ParameterizedTest
+  @CsvSource({"50,L"})
+  void shouldConvertL(int arabicNumber, String romanNumber) {
+    assertThat(ArabicToRomanNumeralsConverter.convert(arabicNumber)).isEqualTo(romanNumber);
+  }
 }
