806799d42d28865489f210d1defb8bed00ba76b1
damien.cognet <damien.cognet@cdbiz.com>
2022-11-29 10:44:44 +0100

    init diamond kata

--- diamond-kata/src/main/java/com/kata/diamond/Diamond.java
+++ diamond-kata/src/main/java/com/kata/diamond/Diamond.java
@@ -1,4 +1,20 @@
 package com.kata.diamond;
 
+import java.util.Locale;
+
 public class Diamond {
+
+  public static String print(char letter) {
+    if (letter == 'a') {
+      letter = toUpperCase(letter);
+    }
+    if (letter < 65 || letter > 90) {
+      return "";
+    }
+    return "A";
+  }
+
+  private static char toUpperCase(char letter) {
+    return String.valueOf(letter).toUpperCase(Locale.ROOT).charAt(0);
+  }
 }
--- diamond-kata/src/test/java/com/kata/diamond/DiamondTests.java
+++ diamond-kata/src/test/java/com/kata/diamond/DiamondTests.java
@@ -1,14 +1,34 @@
 package com.kata.diamond;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class DiamondTests {
-    @Test
-    void test() {
-        assertThat("Diamond")
-                .startsWith("D")
-                .endsWith("d");
-    }
+
+  @Test
+  void should_print_A_when_letter_is_A() {
+    String diamond = Diamond.print('A');
+
+    assertThat(diamond).isEqualTo("A");
+  }
+
+  @ParameterizedTest
+  @ValueSource(chars = {'$', '€'})
+  void should_print_empty_string_when_input_is_not_a_letter(char input) {
+    String diamond = Diamond.print(input);
+
+    assertThat(diamond).isEmpty();
+  }
+
+  @Test
+  void should_print_A_when_letter_is_a() {
+    String diamond = Diamond.print('a');
+
+    assertThat(diamond).isEqualTo("A");
+  }
+
+
 }
