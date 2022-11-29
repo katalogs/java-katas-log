8337d556724ff2de78bf42c73d090d5b8b952705
Nicolas Luvison <nicolas.luvison@cdiscount.com>
2022-11-29 14:39:57 +0100

    First commit

--- gilded-rose-kata/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gilded-rose-kata/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,10 +9,18 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
     }
 
+    @Test
+    void golden() {
+        String[] args = new String[3];
+        //run
+        TexttestFixture.main(args);
+        //assert
+        Approvals.verifyAll("", );
+    }
 }
