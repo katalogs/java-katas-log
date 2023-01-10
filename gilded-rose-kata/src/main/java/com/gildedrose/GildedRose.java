24aff3e487e18a4c3dd32012a4073778228d2626
Mohamed El Youssefy <mohamed.elyoussefy@cdbdx.biz>
2023-01-10 14:59:09 +0100

    liftup conditional on BACKSTAGE_ITEM

--- gilded-rose-kata/src/main/java/com/gildedrose/GildedRose.java
+++ gilded-rose-kata/src/main/java/com/gildedrose/GildedRose.java
@@ -2,59 +2,57 @@ package com.gildedrose;
 
 class GildedRose {
 
-    Item[] items;
+  Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+  public GildedRose(Item[] items) {
+    this.items = items;
+  }
 
-    public void updateQuality() {
-        for (Item item : items) {
-            if (item.name.equals("Aged Brie")) {
-                if (!item.isNormalMaxQuality()) {
-                    item.increaseQuality();
-                }
+  public void updateQuality() {
+    for (Item item : items) {
+      if (item.name.equals("Aged Brie")) {
+        if (!item.isNormalMaxQuality()) {
+          item.increaseQuality();
+        }
 
-                item.sellIn--;
+        item.sellIn--;
 
-                if (item.sellIn < 0) {
-                    item.increaseQuality();
-                }
-            } else {
-                if (!item.name.equals(Item.BACKSTAGE_ITEM)) {
-                    item.decreaseQuality();
-                } else {
-                    if (!item.isNormalMaxQuality()) {
-                        item.increaseQuality();
+        if (item.sellIn < 0) {
+          item.increaseQuality();
+        }
+      } else {
+        if (item.name.equals(Item.BACKSTAGE_ITEM)) {
+          if (!item.isNormalMaxQuality()) {
+            item.increaseQuality();
 
-                        if (item.name.equals(Item.BACKSTAGE_ITEM)) {
-                            if (item.sellIn < 11) {
-                                item.increaseQuality();
-                            }
-
-                            if (item.sellIn < 6) {
-                                item.increaseQuality();
-                            }
-                        }
-                    }
-                }
-
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.sellIn--;
-                }
-
-                if (item.sellIn < 0) {
-                    if (!item.name.equals(Item.BACKSTAGE_ITEM)) {
-                        item.decreaseQuality();
-                    } else {
-                        item.quality = 0;
-                    }
-                }
+            if (item.sellIn < 11) {
+              item.increaseQuality();
             }
 
+            if (item.sellIn < 6) {
+              item.increaseQuality();
+            }
+          }
 
+          item.sellIn--;
+
+          if (item.sellIn < 0) {
+            item.quality = 0;
+          }
+        } else {
+          item.decreaseQuality();
+
+          if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn--;
+          }
+
+          if (item.sellIn < 0) {
+            item.decreaseQuality();
+          }
         }
+      }
     }
+  }
 
 
 }
