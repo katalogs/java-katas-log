f55d1a5511d92c75d628322bd3d65b28cb98398e
Marina Delas <marina.delas@ext.cdiscount.com>
2023-01-10 14:45:42 +0100

    liftup conditional round 1

--- gilded-rose-kata/src/main/java/com/gildedrose/GildedRose.java
+++ gilded-rose-kata/src/main/java/com/gildedrose/GildedRose.java
@@ -10,40 +10,49 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                && !item.name.equals(Item.BACKSTAGE_ITEM)) {
-                item.decreaseQuality();
-            } else {
+            if (item.name.equals("Aged Brie")) {
                 if (!item.isNormalMaxQuality()) {
                     item.increaseQuality();
+                }
 
-                    if (item.name.equals(Item.BACKSTAGE_ITEM)) {
-                        if (item.sellIn < 11) {
-                            item.increaseQuality();
-                        }
+                item.sellIn--;
 
-                        if (item.sellIn < 6) {
-                            item.increaseQuality();
+                if (item.sellIn < 0) {
+                    item.increaseQuality();
+                }
+            } else {
+                if (!item.name.equals(Item.BACKSTAGE_ITEM)) {
+                    item.decreaseQuality();
+                } else {
+                    if (!item.isNormalMaxQuality()) {
+                        item.increaseQuality();
+
+                        if (item.name.equals(Item.BACKSTAGE_ITEM)) {
+                            if (item.sellIn < 11) {
+                                item.increaseQuality();
+                            }
+
+                            if (item.sellIn < 6) {
+                                item.increaseQuality();
+                            }
                         }
                     }
                 }
-            }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn--;
-            }
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.sellIn--;
+                }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
+                if (item.sellIn < 0) {
                     if (!item.name.equals(Item.BACKSTAGE_ITEM)) {
                         item.decreaseQuality();
                     } else {
                         item.quality = 0;
                     }
-                } else {
-                    item.increaseQuality();
                 }
             }
+
+
         }
     }
 
