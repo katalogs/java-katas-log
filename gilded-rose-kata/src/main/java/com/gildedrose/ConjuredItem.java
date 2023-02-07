158206c14696eff79c717b6e43470b8b27074dc3
Nicolas Luvison <nicolas.luvison@cdiscount.com>
2023-02-07 14:42:16 +0100

    Create conjuredItem class

---
+++ gilded-rose-kata/src/main/java/com/gildedrose/ConjuredItem.java
@@ -0,0 +1,8 @@
+package com.gildedrose;
+
+public class ConjuredItem extends Item {
+
+    public ConjuredItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+}
