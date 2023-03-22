d201223ffcc051028e7af8c26da57fd2207d8194
Imane BOUAROUROU <imane.bouarourou.partner@decathlon.com>
2023-03-22 11:08:44 +0100

    add TU

--- gilded-rose-kata/Java 11/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gilded-rose-kata/Java 11/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,6 +2,11 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.StringWriter;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
@@ -13,5 +18,19 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
     }
+
+    @Test
+    void updateQualityTest() throws FileNotFoundException {
+        // execute main
+        StringWriter stringWriter = new StringWriter();
+        //System.setOut(stringWriter.);
+        TexttestFixture.main(null);
+        // add an item
+        // update quality
+        // verify name
+        // verify quality
+        // verify sellIn
+    }
+
 
 }
