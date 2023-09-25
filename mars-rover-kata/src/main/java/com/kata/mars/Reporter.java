e03dcac82cb190c562afdef8188ce112d225a0d5
Clément Barbazange <clement.barbazange@cdbdx.biz>
2023-09-25 15:15:02 +0200

    Refacto : fixing NPE

--- mars-rover-kata/src/main/java/com/kata/mars/Reporter.java
+++ mars-rover-kata/src/main/java/com/kata/mars/Reporter.java
@@ -5,10 +5,10 @@ public class Reporter {
   public String report(Sequence sequence, Position position, Direction direction) {
 
     String prefix = "";
-    if (sequence.getError().getClass().equals(ObstacleError.class)) {
+    if (sequence.getError() != null && sequence.getError().getClass().equals(ObstacleError.class)) {
       prefix = "O:";
     }
-    if (sequence.getError().getClass().equals(CommandError.class)) {
+    if (sequence.getError() != null && sequence.getError().getClass().equals(CommandError.class)) {
       prefix = "E:";
     }
     return prefix + (position.x() + ":" + position.y() + ":") + direction;
